728fada438c8c7dc53295ce3af170724ed6b22d3
Husam Abuhabib <hmabuhabib@gmail.com>
2021-01-14 10:38:45 +0100

    Chatbot
    add kommunicate plugins for Sandy chatbot

--- Project-2/js/app.js
+++ Project-2/js/app.js
@@ -99,3 +99,14 @@ for (let i = 0; i < acc.length; i++) {
  });
 }
 
+(function (d, m) {
+ var kommunicateSettings =
+  { "appId": "e37c337240cf460b2d79ddafee541516", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+ var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+ s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+ var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+ window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+
+
