7e813b0581fab1357ee6d6e40deb87ccb99f8728
Husam Abuhabib <hmabuhabib@gmail.com>
2021-01-14 10:51:48 +0100

    Update app.js
    add AudioContext

--- Project-2/js/app.js
+++ Project-2/js/app.js
@@ -99,14 +99,18 @@ for (let i = 0; i < acc.length; i++) {
  });
 }
 
-(function (d, m) {
- var kommunicateSettings =
-  { "appId": "e37c337240cf460b2d79ddafee541516", "popupWidget": true, "automaticChatOpenOnNavigation": true };
- var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
- s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
- var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
- window.kommunicate = m; m._globals = kommunicateSettings;
-})(document, window.kommunicate || {});
+let context = new AudioContext();
+//on iOS, the context will be started on the first valid user action on the #playButton element
+StartAudioContext(context, "#playButton")
+
+ (function (d, m) {
+  var kommunicateSettings =
+   { "appId": "e37c337240cf460b2d79ddafee541516", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+  var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+  s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+  var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+  window.kommunicate = m; m._globals = kommunicateSettings;
+ })(document, window.kommunicate || {});
 /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
 
 
