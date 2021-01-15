e35eb588e17f3cf55a583204779defec0cdb4106
Husam Abuhabib <hmabuhabib@gmail.com>
2021-01-15 20:32:03 +0100

    change the files

--- Project-3/server.js
+++ Project-3/server.js
@@ -1,19 +1,55 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+/* Empty JS object to act as endpoint for all routes */
+projectData = [];
 
-// Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* Dependencies */
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
-// Initialize the main project folder
+/* Initializing the main project folder */
 app.use(express.static('website'));
 
+const port = 5500;
+const server = app.listen(port, listening);
+function listening() {
+ // console.log(server);
+ console.log(`running on localhost: ${port}`);
+};
+// TODO-Spin up the server
 
-// Setup Server
+app.get('/all', function (request, response) {
+ response.send(projectData)
+ console.log("get sending the Data back")
+}
+);
+
+
+app.post('/addData', function (req, res) {
+ console.log(req.body);
+ newEntry = {
+  temperature: req.body.temperature,
+  date: req.body.date,
+  feelingsField: req.body.user_response
+ }
+ projectData.push(newEntry)
+ res.send()
+ console.log(projectData)
+});
+
+
+
+
+
+
+
--- Project-3/website/app.js
+++ Project-3/website/app.js
@@ -1,5 +1,126 @@
 /* Global Variables */
 
+//const { error } = require("console");
+
 // Create a new date instance dynamically with JS
+var currentdate = new Date();
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+ + (currentdate.getMonth() + 1) + "/"
+ + currentdate.getFullYear() + " @ "
+ + currentdate.getHours() + ":"
+ + currentdate.getMinutes() + ":"
+ + currentdate.getSeconds();
+
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear() + " Time:" + d.getHours() + ":"
+ + d.getMinutes();
+let zipField = document.querySelector('#zip');
+const API_Key = '4964517d2628ee5cfcffcfe8021efd92';
+let feelingsField = document.querySelector('#feelings');
+let allData = [{ 'SAMER': 15 }];
+const cards = document.querySelector('.entry');
+const generateBtn = document.querySelector('#generate');
+generateBtn.addEventListener('click', function (e) {
+ if (zipField.value) {
+  console.log(zipField.value);
+  console.log(feelingsField.value);
+  getWeathermapData(zipField.value);
+ } else
+  console.log("Error");
+
+});
+
+
+// fetch the data from the server 
+const getAlldata = async () => {
+
+ const res = await fetch(`http://localhost:5500/all`)
+
+ try {
+  const data = await res.json();
+
+  return data;
+ } catch (error) {
+  console.log("Error in getAlldata", error);
+  // appropriately handle the error
+ }
+}
+
+
+
+// fetch the data from Weathermap website
+const getWeathermapData = async (zip) => {
+
+ try {
+  const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_Key}&units=metric`)
+  const data = await res.json();
+
+  if (data.cod == 200) {
+   postData('http://localhost:5500/addData', { temperature: data, date: newDate, user_response: feelingsField.value });
+   getAlldata()
+    .then(data => updateUI(data))
+    .then(
+     console.log("Everything went fine")
+    )
+    .catch("Error in then fun");
+   return data;
+  } else {
+   alert(data.message);
+   console.log(data.message);
+   return data.message;
+  }
+
+
+ } catch (error) {
+  console.log("Error in getWeathermapData", error);
+ }
+};
+
+
+// Post the data to server side
+
+const postData = async (url = '', data = {}) => {
+ //console.log(data);
+ const response = await fetch(url, {
+  method: 'POST',
+  credentials: 'same-origin',
+  headers: {
+   'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+ });
+
+ try {
+  const newData = await response.json();
+  console.log(newData);
+  return newData;
+ } catch (error) {
+  console.log("error", error);
+ }
+}
+
+const activeCard = document.querySelector('.active');
+const entry = document.querySelector('.entry');
+
+
+function updateUI(data) {
+ const element = document.createElement('div');
+ if (entry.children.length > 0) {
+  const activeCard = document.querySelector('.active');
+  activeCard.classList.remove("active");
+ }
+
+ element.innerHTML = `  <div class="weatherCard active" id="entryHolder">
+ <h4>Current Weather in ${data[data.length - 1].temperature.name} , ${data[data.length - 1].temperature.sys.country}</h4>
+<div id="date:">Date: ${data[data.length - 1].date}</div>
+<div id="temp:">Temperatur: ${data[data.length - 1].temperature.main.temp}</div>
+<div id="content">Feelings: ${data[data.length - 1].feelingsField}</div>
+<img id="openweathermap_icon" src="http://openweathermap.org/img/wn/${data[data.length - 1].temperature.weather[0].icon}@2x.png" alt="openweathermap icon">
+</div>`;
+ entry.insertBefore(element, entry.firstChild);
+ console.log(entry.children[0]);
+ console.log(data[data.length - 1].temperature.main.temp);
+ console.log(data[data.length - 1]);
+
+}
+
